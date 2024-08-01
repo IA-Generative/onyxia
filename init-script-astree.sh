8b045712f81a13889347140044bdd7a3ab8d8f8a
victorjourne <journe.victor@gmail.com>
2024-08-01 15:54:38 +0200

    Create init-script-astree.sh

---
+++ init-script-astree.sh
@@ -0,0 +1,29 @@
+sudo apt update
+sudo apt install rclone fuse3
+mkdir -p /home/onyxia/.config/rclone
+
+
+# Add S3 rclone bucket [ovh-snc]
+cat << EOF > /home/onyxia/.config/rclone/rclone.conf 
+[ovh-snc]
+type = s3
+provider = Minio
+env_auth = true
+endpoint = $AWS_S3_ENDPOINT
+region = $AWS_DEFAULT_REGION
+acl = private
+server_side_encryption = aws:kms
+sse_kms_key_id = minio-kms-key
+upload_cutoff = 0
+
+[secret]
+type = crypt
+remote = ovh-snc:vjourne-astree
+filename_encryption = obfuscate
+EOF
+
+rclone config password secret password $CRYPT_PASSWORD
+
+# Mount data
+mkdir ./data
+rclone mount secret:vjourne-astree ./data --daemon
