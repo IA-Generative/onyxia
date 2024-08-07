e073e9b38da5cbc39a5cafe3711ab2111ae3ec40
victorjourne <journe.victor@gmail.com>
2024-08-07 15:52:23 +0200

    Create init-script-marcus.sh

---
+++ init-script-marcus.sh
@@ -0,0 +1,11 @@
+# Install vscode extension
+code-server --install-extension privy.privy-vscode
+# Add privy conf to vscode
+jq '. + {
+  "privy.providerBaseUrl": "https://ollama.c1.cloud-pi-native.com/",
+  "privy.autocomplete.model": "deepseek-coder:1.3b-base",
+  "privy.model": "custom",
+  "privy.customModel": "gemma2:27b"
+}' /home/onyxia/.local/share/code-server/User/settings.json > /home/onyxia/.local/share/code-server/User/settings_tmp.json
+mv /home/onyxia/.local/share/code-server/User/settings_tmp.json /home/onyxia/.local/share/code-server/User/settings.json
+
