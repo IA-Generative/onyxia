c25480d9cfccd02eb9d61ca5445a63a372964fc9
victorjourne <journe.victor@gmail.com>
2024-08-12 16:00:30 +0200

    Create init-descartes.sh

---
+++ init-descartes.sh
@@ -0,0 +1,12 @@
+curl -sSL https://raw.githubusercontent.com/IA-Generative/onyxia/main/init-script-marcus.sh | bash
+
+#  FOr the python app
+sudo apt-get update
+sudo apt-get install libgl1-mesa-glx
+
+#  For the frontend
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# download and install Node.js (you may need to restart the terminal)
+nvm install 20S
+
