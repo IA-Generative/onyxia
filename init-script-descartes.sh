ca4e1899a45af4fe9a5573bdfdea3443aa10e56c
victorjourne <journe.victor@gmail.com>
2024-08-13 09:53:11 +0200

    Update init-script-descartes.sh

--- init-script-descartes.sh
+++ init-script-descartes.sh
@@ -7,6 +7,9 @@ sudo apt-get install libgl1-mesa-glx
 #  For the frontend
 # installs nvm (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-# download and install Node.js (you may need to restart the terminal)
-nvm install 20S
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 20
+
 
