d083f60383ff995a93b2bed8c341c1918761e66b
victorjourne <journe.victor@gmail.com>
2024-09-02 19:22:57 +0200

    Update init-script-technical-test.sh

--- init-script-technical-test.sh
+++ init-script-technical-test.sh
@@ -1,9 +1,9 @@
 sudo apt update
 
+git clone https://github.com/IA-Generative/technical-tests.git -b r2i-extraction
+
 # Create .env
-cat << EOF > /home/onyxia/work/.env
+cat << EOF > /home/onyxia/work/technical-tests/.env
 OPENAI_API_BASE=https://api-ai.numerique-interieur.com/v1
 OPENAI_API_KEY=$PERSONAL_INIT_ARGS
 EOF
-
-git clone https://github.com/IA-Generative/technical-tests.git -b r2i-extraction
