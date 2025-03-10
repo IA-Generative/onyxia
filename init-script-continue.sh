30a2a7ba186268bbd3cc1a42fd2ca44fea7ff953
victorjourne <journe.victor@gmail.com>
2025-03-10 16:18:08 +0100

    Update init-script-continue.sh

--- init-script-continue.sh
+++ init-script-continue.sh
@@ -1,5 +1,6 @@
 code-server --install-extension Continue.continue
 mkdir -p /home/onyxia/.continue/
 curl -s -o /home/onyxia/.continue/config.json https://raw.githubusercontent.com/IA-Generative/onyxia/refs/heads/main/.continue/config.json
-MY_TMP_VAR=$(echo "$VSCODE_PROXY_URI" | awk -F'.' '{print $3}')
+##MY_TMP_VAR=$(echo "$VSCODE_PROXY_URI" | awk -F'.' '{print $3}')
+MY_TMP_VAR=cloud-pi-native
 sed -i "s|__PLACEHOLDER_OLLAMA_URL__|https://ollama.c1.${MY_TMP_VAR}.com/|g" /home/onyxia/.continue/config.json
