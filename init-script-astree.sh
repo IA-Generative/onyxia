cc44b69c055e64143ada250788623dfe420cd146
Mathieu Guérin <mathieu.guerin@saegus.com>
2025-04-01 10:58:08 +0200

    Run custom user init script after init-script-astree.sh

--- init-script-astree.sh
+++ init-script-astree.sh
@@ -6,7 +6,7 @@ mkdir -p /home/onyxia/.config/rclone
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # Add S3 rclone bucket [scw-astree]
-cat << EOF > /home/onyxia/.config/rclone/rclone.conf 
+cat << EOF > /home/onyxia/.config/rclone/rclone.conf
 [scw-astree]
 type = s3
 provider = Scaleway
@@ -19,3 +19,8 @@ EOF
 mkdir ./data
 rclone mount scw-astree:siaj ./data --daemon
 code-server --install-extension anwar.papyrus-pdf
+
+# Run custom user init script
+if [ -f /home/onyxia/work/.init/init.sh ]; then
+  cat /home/onyxia/work/.init/init.sh | bash
+fi
